d722ae99df66938e5cfe96f2866003ce8c623132
melteresa <meteran@gbmpaulovi.edu.ec>
2025-10-22 16:31:30 -0500

    Agregar main.py
    Realicé un programa de cómo calcular el digito verificador de la cedula ecuatoriana.

---
+++ main.py
@@ -0,0 +1,16 @@
+cedula = "172717393"
+coeficientes = [2,1,2,1,2,1,2,1,2]
+suma_total = 0
+for i in range(9):
+    digito = int(cedula[i])
+    producto = digito * coeficientes[i]
+    if producto >= 10:
+        producto -= 9
+    suma_total += producto
+    #print(suma_total)
+    residuo = suma_total % 10
+    if residuo == 0:
+        digito_verficador = 0
+    else:
+     digito_verificador = 10 - residuo
+print (digito_verficador)
